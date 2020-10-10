5a3085ec9e70fbdb2252abfd6cde413d4e570fbc
nimurarikuto <nimuyohu0105@gmail.com>
2020-10-10 15:29:52 +0900

    遷移追加

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="nav-wrapper green">
+        <a href="/" className="brand-logo center">
+          Tech Quiz
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
+
+
